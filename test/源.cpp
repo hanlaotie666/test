b04f18498ea07a10686c3daf20164cc75dd63429
hanlaotie666 <1337304390@qq.com>
2020-04-22 01:06:50 +0800

    700 git

--- test/源.cpp
+++ test/源.cpp
@@ -3,20 +3,6 @@
 using namespace std;
 int main()
 {
-	cout<<"hello"<<endl;
-	cout<<"new"<<endl;
-	cout<<"hello"<<endl;
-	cout<<"new"<<endl;
-	return 0;
-	return 0；
-	return 0；
-	cout<<"hello"<<endl;
-	cout<<"new"<<endl;
-	cout<<"hello"<<endl;
-	cout<<"new"<<endl;
-	return 0;
-	return 0；
-	return 0；
 }
 #include<iostream>
 #include<vector>
