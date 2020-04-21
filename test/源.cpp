c24247c01599e22e3ea0114480c4c773c259dc0d
unknown <1337304390@qq.com>
2020-04-21 18:06:43 +0800

    second git

--- test/源.cpp
+++ test/源.cpp
@@ -3,41 +3,13 @@ using namespace std;
 int main()
 {
 	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	return 0;
-}
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
+	cout << "1" << endl;
+	cout << 2<< endl;
+	cout << 3<< endl;
+	cout << 4<< endl;
 	cout << "wojinxingxiugai1" << endl;
 	cout << "hello,world" << endl;
 	cout << "wojinxingxiugai1" << endl;
 	return 0;
 }
 
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
-	return 0;
-}
