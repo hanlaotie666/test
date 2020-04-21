8c94cb1346c26062514226428062da4cbde23a08
unknown <1337304390@qq.com>
2020-04-21 18:51:46 +0800

    3 git

--- test/源.cpp
+++ test/源.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 int main()
 {
-	cout << "hello,world" << endl;
-	cout << "1" << endl;
-	cout << 2<< endl;
-	cout << 3<< endl;
-	cout << 4<< endl;
-	cout << "wojinxingxiugai1" << endl;
-	cout << "hello,world" << endl;
-	cout << "wojinxingxiugai1" << endl;
 	return 0;
 }
 
