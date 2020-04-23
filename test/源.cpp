561cc791b5603e80107851b9657f306324b68de8
hanlaotie666 <1337304390@qq.com>
2020-04-23 16:49:11 +0800

    701 git

--- test/源.cpp
+++ test/源.cpp
@@ -1,53 +1,49 @@
-using namespace std;
 #include<iostream>
+#include<unordered_map>
 #include<vector>
-#include<math.h>
-#include<algorithm>
 using namespace std;
-#include<iostream>
-#include<vector>
-#include<math.h>
-#include<algorithm>
-using namespace std;
+
 class Solution {
 public:
-	int n;
-	long long int money;
-	vector<long long int> w;
-	vector<vector<long long int>> dp;
-	int getMin(long long int a, long long int b)
-	{
-		return a > b ? b : a;
-	}
-	int search(int idx, long long int m)
-	{
-		
-		if (idx >= n) return 999999;
-		if (m == money) return 0;
-		if (dp[idx][m] < 99999) return dp[idx][m];
-
-		if ((long long)(m + w[idx]) > money ) 
-			dp[idx][m]=search(idx + 1, m);
-		else
-			dp[idx][m] = getMin(search(idx, m + w[idx]) + 1, search(idx + 1, m));
-		return dp[idx][m];
-
-	}
-	int coinChange(vector<int>& coins, int amount) {
-		money = amount;
-		n = coins.size();
-		for (int i = 0; i < n; i++) w.push_back(coins[i]);
-		vector<vector<long long int>> f(n, vector<long long int>(money, 999999));
-		dp = f;
-		int res = search(0, 0);
-		return res>9999 ? -1 : res;
+	bool checkSubarraySum(vector<int>& nums, int k) {
+		int n = nums.size();
+		if (n == 0) return false;
+		if (k == 0) {
+			for (int i = 1; i < n; i++)
+				if (nums[i - 1] == nums[i] && nums[i] == 0) return true;
+			return false;
+		}
+		vector<int> preSum(n,0);
+		unordered_map<int, vector<int>> MyMap;//key 表示preSum[i] % k,value 表示下标的vector
+		preSum[0] = nums[0];
+		MyMap[preSum[0] % k].push_back(0);
+		MyMap[0].push_back(-1);				//当余数为0时，特殊处理
+		for (int i = 1; i < n; i++)
+		{
+			preSum[i] = preSum[i - 1] + nums[i];
+			MyMap[preSum[i] % k].push_back(i);
+		}
+		unordered_map<int, vector<int>>::iterator it = MyMap.begin();
+		for (; it != MyMap.end(); it++)
+		{
+			if (it->second.size() == 1) continue;
+			else if (it->second.size() >= 3) return true;
+			else
+			{
+				int temp1 = (it->second)[1];
+				int temp2 = (it->second)[0];
+				if (temp1 - temp2 > 1) return true;
+				else continue;
+			}
+		}
+		return false;
 	}
 };
 int main()
 {
-	vector<int> coins{ 1,2147483647 };
-	int amount = 2;
+	vector<int> nums{23,2,6,4,7};
+	int k = 6;
 	Solution sol;
-	cout<<sol.coinChange(coins, 2)<<endl;
+	cout<<sol.checkSubarraySum(nums,k)<<endl;
 	return 0;
 }
