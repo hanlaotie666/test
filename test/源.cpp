3d0f90223861e5487a23b0d2ef19e35a6229f252
hanlaotie666 <1337304390@qq.com>
2020-04-25 21:07:32 +0800

    701 git

--- test/源.cpp
+++ test/源.cpp
@@ -1,32 +1,26 @@
 #include<iostream>
-#include<string>
+#include<queue>
 using namespace std;
-class Solution {
-public:
-	int Exist(int len, string s)
-	{
-		for (int i = 0; i + len <= s.size(); i++)
-		{
-			string s_temp(s.begin() + i, s.begin() + i + len);
-			string s_temp2(s_temp);
-			reverse(s_temp2.begin(), s_temp2.end());
-			if (s_temp.compare(s_temp2) == 0) return i;
-		}
-		return -1;
-	}
-	string longestPalindrome(string s) {
-		int len = s.size();
-		for (; len >= 1; len--)
-			if (Exist(len, s) != -1) break;
-		int  i = Exist(len, s);
-		string res(s.begin()+i,s.begin()+i+len);
-		return res;
-	}
-};
 int main()
 {
-	string s;
-	cin >> s;
-	Solution sol;
-	cout << sol.longestPalindrome(s) << endl;
+	int a, b;
+	cin >> a >> b;
+	queue<pair<pair<int,int>,int>> myque; //存储 第 i 层  j 操作  a当前的值
+	myque.push(pair<pair<int,int>,int>(pair<int,int>(0,0),a+1)); 
+	myque.push(pair<pair<int, int>, int>(pair<int, int>(0, 1), a-1));
+	myque.push(pair<pair<int, int>, int>(pair<int, int>(0, 2), a*2));
+	int res = 0;
+	while (!myque.empty())
+	{
+		pair<pair<int, int>, int> opera = myque.front();
+		myque.pop();
+		int now_a = opera.second;
+		if (now_a == b) { res = opera.first.first + 1; break; }
+		for (int i = 0; i < 3; i++)
+			if(i==0) myque.push(pair<pair<int, int>, int>(pair<int,int>(opera.first.first + 1, i), now_a+1));
+			else if(i==1) myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a-1));
+			else myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a*2));
+	}
+	cout << res << endl;
+	return 0;
 }
