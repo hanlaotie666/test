caa390c92367f7e663f8a5e04c73613ef3dc57bb
hanlaotie666 <1337304390@qq.com>
2020-04-25 21:59:36 +0800

    701 git

--- test/源.cpp
+++ test/源.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int a, b;
 	cin >> a >> b;
-	queue<pair<pair<int,int>,int>> myque; //存储 第 i 层  j 操作  a当前的值
+	queue<pair<pair<int,int>,int>> myque; //	{(i,j),now_a} 存储 第 i 层  j 操作  a当前的值
 	myque.push(pair<pair<int,int>,int>(pair<int,int>(0,0),a+1)); 
 	myque.push(pair<pair<int, int>, int>(pair<int, int>(0, 1), a-1));
 	myque.push(pair<pair<int, int>, int>(pair<int, int>(0, 2), a*2));
@@ -17,9 +17,11 @@ int main()
 		int now_a = opera.second;
 		if (now_a == b) { res = opera.first.first + 1; break; }
 		for (int i = 0; i < 3; i++)
-			if(i==0) myque.push(pair<pair<int, int>, int>(pair<int,int>(opera.first.first + 1, i), now_a+1));
-			else if(i==1) myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a-1));
-			else myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a*2));
+		{
+			if (i == 0) myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a + 1));
+			if (i == 1) myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a - 1));
+			if (i == 2) myque.push(pair<pair<int, int>, int>(pair<int, int>(opera.first.first + 1, i), now_a * 2));
+		}
 	}
 	cout << res << endl;
 	return 0;
