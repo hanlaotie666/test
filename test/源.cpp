3665ecd140863405979f9b44e7e0254843112395
hanlaotie666 <1337304390@qq.com>
2020-05-06 20:51:08 +0800

    009

--- test/源.cpp
+++ test/源.cpp
@@ -1,50 +1,117 @@
-import sys
-import pdb
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<numeric>
+using namespace std;
+vector<vector<int>> allpaths;
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+int res=-99999999;
+TreeNode* Build(string str)
+{
+	TreeNode* root = new TreeNode(-99999999);
+	//判断有无括号
+	int tempi = -1;
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == '(') { tempi = i; break; }
+	}
+	if (tempi != -1)//有'('
+	{
+		string stemp = str.substr(0, tempi);
+		int a = atoi(stemp.c_str());
+		root->val = a;
+		while ((tempi--) >= 0) str.erase(str.begin());
+		str.erase(str.end() - 1);
+		//找','号
+		string::size_type  mypos = str.find(",");
+		if (str.find(",") != str.npos)
+		{
+			string str1 = str.substr(0, mypos);
+			string str2 = str.substr(mypos+1);
+			root->left = Build(str1);
+			root->right = Build(str2);
+			return root;
+		}
+		else
+		{
+			int a = atoi(str.c_str());
+			root->val = a;
+			return root;
+		}
 
-tree=input().strip()
-sum=0
-max_sum=0
-index=0
-op=None
-path=[]
-index1,index2,index3=len(tree),len(tree),len(tree)
-while(len(tree)>0):
-    index1, index2, index3 = len(tree), len(tree), len(tree)
-    if '(' in tree:
-        index1=tree.index('(')
-    if ')' in tree:
-        index2=tree.index(')')
-    if ',' in tree:
-        index3=tree.index(',')
-    index = min(index1,index2,index3)
-    if index!=0:
-        val = int("".join(x for x in tree[:index]))
-        if not op:
-            sum += val
-        else:
-            if op == '(':
-                sum += val
-            elif op == ')':
-                sum -= path[-1]
-                path.pop(-1)
-                sum += val
-            elif op == ',':
-                sum -= path[-1]
-                path.pop(-1)
-                sum += val
-        path.append(val)
-    else:
-        sum-=path[-1]
-        path.pop(-1)
-    if index==index1:
-        op='('
-    elif index==index2:
-        op=')'
-    elif index==index3:
-        op=','
-    if sum<0:
-        sum=0
-    if max_sum<sum:
-        max_sum=sum
-    tree=tree[index+1:]
-print(max_sum)
+	}
+	else
+	{
+		string::size_type  mypos = str.find(",");
+		if (str.find(",") != str.npos)
+		{
+			string str1 = str.substr(0, mypos + 1);
+			string str2 = str.substr(mypos);
+			root->left = Build(str1);
+			root->right = Build(str2);
+			return root;
+		}
+		else
+		{
+			int a = atoi(str.c_str());
+			root->val = a;
+			return root;
+		}
+	}
+	return NULL;
+}
+vector<vector<int> > allPath;
+//找到所有路径
+void findAllPath(TreeNode* pRoot, vector<int>&path) {
+	if (pRoot == NULL)
+	{
+		return;
+	}
+	path.push_back(pRoot->val);
+	if (pRoot->left == NULL&&pRoot->right == NULL) //达到了叶子节点
+	{
+		allPath.push_back(path);//保存路径
+	}
+	if (pRoot->left != NULL)//左子树
+	{
+		findAllPath(pRoot->left, path);
+	}
+	if (pRoot->right != NULL)//右子树
+	{
+		findAllPath(pRoot->right, path);
+	}
+	path.pop_back();//在返回到父节点之前，在路径上删除当前节点
+}
+int maxSubArray(vector<int>& nums) {
+	int ans = 0;
+	int max = -2147483647;
+	for (int i = 0; i<nums.size(); i++)
+	{
+		if (ans<0) ans = 0;
+		ans = ans + nums[i];
+		if (ans>max)
+			max = ans;
+	}
+	return max;
+}
+
+int main()
+{
+	string temp;
+	cin >> temp;
+	TreeNode* root =Build(temp);
+	vector<int> path;
+	findAllPath(root,path);
+	for (int i = 0; i < allPath.size(); i++)
+	{
+		int temp = maxSubArray(allPath[i]);
+		res = res > temp ? res : temp;
+	}
+	cout << res << endl;
+	return 0;
+}
