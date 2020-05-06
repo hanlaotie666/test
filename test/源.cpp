02202f61a6b3156fb077ca160d104a24a145b031
hanlaotie666 <1337304390@qq.com>
2020-05-06 19:30:56 +0800

    006

--- test/源.cpp
+++ test/源.cpp
@@ -3,30 +3,44 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-int res;
-vector<vector<int>> dp(3000, vector<int>(3000, -1));
-int search(int n, int m)
+bool cmp(const pair<int, int>& a, const pair<int, int>& b)
 {
-	if (dp[n][m] >= 0) return dp[n][m];
-	int temp;
-	if (n < m) temp = 0;
-	else if (m == 0) temp=1;
-	else if (m == n) temp = 1;
-	else
-		temp= search(n - 1, m - 1) + search(n - 1, m);
-	temp = temp % (1000000000 + 7);
-	dp[n][m] = temp;
-	return temp;
+	return a.second > b.second;
 }
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++)
+	int M, N;
+	cin >> M >> N;
+	vector<int> host(M,0);
+	for (int i = 0; i < M; i++) cin >> host[i];
+	sort(host.begin(), host.end());
+	int tempmax = host[M-1];
+	vector<pair<int, int>> req;
+	for (int i = 0; i < N; i++)
 	{
-		int temp = search(n - i + m - 1-1, n - i);
-		temp = temp % (1000000000 + 7);
-		res = (res + temp)%(1000000000 + 7);
+		int temp1, temp2; cin >> temp1 >> temp2;
+		if (temp1 <= tempmax) {
+			pair<int, int> pa;
+			pa.first = temp1; pa.second=temp2;
+			req.push_back(pa);
+		}			
+	}
+	sort(req.begin(), req.end(), cmp);
+	int res = 0;
+	for (int i = 0; i < req.size(); i++)
+	{
+		int tempj = -1;
+		for (int j = 0; j < host.size(); j++)
+		{
+			if (host[j] >= req[i].first) { tempj = j; break; }
+		}
+		if (tempj != -1)
+		{
+			res += req[i].second;
+			host.erase(host.begin() + tempj);
+		}
+
 	}
 	cout << res << endl;
+	return 0;
 }
